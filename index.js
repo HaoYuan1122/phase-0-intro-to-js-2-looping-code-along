8718d3a1abcb8759bf02d9928c58139adf78d70e
Hao Yuan <haomichaelyuan@gmail.com>
2022-09-04 19:23:44 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const newArray=[]
+
+function writeCards(names,events){
+    for(let i=0;i<names.length;i++){
+        newArray[i]=`Thank you, ${names[i]}, for the wonderful ${events} gift!`
+    }
+    return newArray
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday")
+console.log(newArray)
+
+function countDown(a){
+    while(a>=0){
+        console.log(a)
+        a--
+    }
+}
